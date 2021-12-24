c501d326626fd39cc508d2ef8b1d9ced2f76c7ad
kckc0608 <kckc0608@naver.com>
2021-12-24 12:58:41 +0900

    작성한 데이터 그리드 데이터를 엑셀로 내보내자

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,6 +20,11 @@ namespace GridPractice
 {
     public partial class MainWindow : Window
     {
+        Excel.Application app;
+        Excel.Workbook workbook;
+        Excel.Worksheet worksheet;
+        Excel.Range range;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -36,6 +41,50 @@ namespace GridPractice
 
             //dataGrid.DataContext = rowdataList; // 이걸로 해도 되더라 단, xaml에서 ItemsSource = "{Binding}" 을 해주어야 함. 무슨 차이인지 모르겠음.
             dataGrid.ItemsSource = rowdataList; // 이걸 할때는 단독으로 이거만 쓰면 됨.
+
+            this.app = new Excel.Application();
+            // 새 워크북 생성
+            //this.workbook = app.Workbooks.Add();
+
+            // 기존 워크북 열기
+            this.workbook = app.Workbooks.Open(@"C:\Users\W24880\Documents\test.xlsx");
+            this.worksheet = workbook.Worksheets.Item["sheet1"] as Excel.Worksheet;
+
+            //this.worksheet = (Excel.Worksheet)workbook.Worksheets.Add(); // 명시적으로 형변환을 안하면 오류가 난다고..?
+        }
+
+        private void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            this.app.Visible = true;
+
+            // 선택한 셀에 대해 값 쓰기
+            range = worksheet.Cells[1,1] as Excel.Range;
+            range.Value = 1;
+
+            // 범위로 지정한 열에 대해 같은 값을 한번에 쓰기 (2,2,2 가 써짐)
+            range = worksheet.Range["A2:C2"] as Excel.Range;
+            range.Value = 2;
+
+            // 그리드에 있는 값을 엑셀에 적기
+            int row = 3;
+            foreach (RowData item in dataGrid.Items.OfType<RowData>())
+            {
+                string text = item.text;
+                bool isChecked = item.isChecked;
+                string status = item.status.ToString();
+
+                Excel.Range range1 = worksheet.Cells[row, 1] as Excel.Range;
+                Excel.Range range2 = worksheet.Cells[row, 2] as Excel.Range;
+                Excel.Range range3 = worksheet.Cells[row, 3] as Excel.Range;
+                range1.Value = text;
+                range2.Value = isChecked;
+                range3.Value = status;
+                row ++;
+            }
+
+            // 추가한 데이터를 저장한다. 닫을 때 저장하시겠습니까? 안뜸! (와우..)
+            workbook.Save();
+            this.Close();
         }
     }
 
--- run.cmd
+++ run.cmd
@@ -3,4 +3,20 @@
 :: 명령어를 실행하고, 배치프로그램이 바로 종료되지 않도록 Pause 명령어로 일시중지 시킨다.
 @echo off
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\MSBuild datagrid_practice.csproj
+
+rem rem 은 주석 지시자
+:: ::도 주석 지시자
+
+set BUILD_STATUS=%ERRORLEVEL%
+if %BUILD_STATUS% == 0 goto end
+if not %BUILD_STATUS% == 0 goto fail
+
+:: exit /b 0 에서 exit 은 종료 명령어고, /b 옵션은 cmd가 아니라 배치프로그램을 종료하겠다는 의미, 0 은 명시적으로 넘겨주는 종료값.
+
+:end
 start bin\Debug\datagrid_practice.exe
+exit /b 0
+
+:fail
+echo %BUILD_STATUS%
+exit /b 0
